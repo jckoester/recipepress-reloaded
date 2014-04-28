53c3dac5dd94b98bd87962cc97455b2689915421
dasmaeh <dasmaeh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-28 17:38:48 +0000

    fixed a translation error and a minor bug in ui
    git-svn-id: http://plugins.svn.wordpress.org/recipepress-reloaded/trunk@904252 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/metabox_details.php
+++ templates/metabox_details.php
@@ -37,23 +37,23 @@ $featured= get_post_meta( $recipe->ID, "rpr_recipe_featured", true );
      </div>-->
      <div class="recipe_details_row rpr_servings">
      	<label for="rpr_recipe_servings"><?php _e( 'Servings', $this->pluginName ); ?></label>
-     	<input type="number" name="rpr_recipe_servings" id="rpr_recipe_servings" value="<?php echo $servings; ?>" />
+     	<input type="number" min="0" name="rpr_recipe_servings" id="rpr_recipe_servings" value="<?php echo $servings; ?>" />
         <input type="text" name="rpr_recipe_servings_type" id="rpr_recipe_servings_type" value="<?php echo $servings_type; ?>" />
-        <span class="recipe-general-form-notes" id="servings_note"> <?php _e( '(e.g. 2 people, 3 loafs, ...)', $this->pluginName ) ?></span>
+        <div class="recipe-general-form-notes" id="servings_note"> <?php _e( '(e.g. 2 people, 3 loafs, ...)', $this->pluginName ) ?></div>
      </div>
      <div class="recipe_details_row">
      	<label for="rpr_recipe_prep_time"><?php _e( 'Prep Time', $this->pluginName ); ?></label>
-     	<input type="number" name="rpr_recipe_prep_time" id="rpr_recipe_prep_time" value="<?php echo $prep_time; ?>" />
+     	<input type="number" min="0"  name="rpr_recipe_prep_time" id="rpr_recipe_prep_time" value="<?php echo $prep_time; ?>" />
         <span class="recipe-general-form-notes"> <?php _e( 'min.', $this->pluginName ) ?></span>
      </div>
      <div class="recipe_details_row">
      	<label for="rpr_recipe_cook_time"><?php _e( 'Cook Time', $this->pluginName ); ?></label>
-     	<input type="number" name="rpr_recipe_cook_time" id="rpr_recipe_cook_time" value="<?php echo $cook_time; ?>" />
+     	<input type="number" min="0"  name="rpr_recipe_cook_time" id="rpr_recipe_cook_time" value="<?php echo $cook_time; ?>" />
         <span class="recipe-general-form-notes"> <?php _e( 'min.', $this->pluginName ) ?></span>
      </div>
      <div class="recipe_details_row">
      	<label for="rpr_recipe_passive_time"><?php _e( 'Passive Time', $this->pluginName ); ?></label>
-     	<input type="number" name="rpr_recipe_passive_time" id="rpr_recipe_passive_time" value="<?php echo $passive_time; ?>" />
+     	<input type="number" min="0"  name="rpr_recipe_passive_time" id="rpr_recipe_passive_time" value="<?php echo $passive_time; ?>" />
         <span class="recipe-general-form-notes"> <?php _e( 'min.', $this->pluginName ) ?></span>
      </div>
      <div class="recipe_details_row">
