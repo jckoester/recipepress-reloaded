42f091b198d9684493107fc2235c55d8c5cc528b
dasmaeh <dasmaeh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-30 16:12:37 +0000

    Hopefully fixed the query problem
    git-svn-id: http://plugins.svn.wordpress.org/recipepress-reloaded/trunk@999272 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/class/rpr_core.php
+++ php/class/rpr_core.php
@@ -224,8 +224,15 @@ class RPR_Core extends RPReloaded {
                 }
             }
             
-            if ( is_home() && $query->is_main_query() ){
-            	$query->set( 'post_type', array( 'post', 'rpr_recipe') );
+            if ( $query->is_main_query() ){
+            	$post_type = $query->get('post_type');
+            	
+            	if( is_array( $post_type ) && ! array_key_exists( 'rpr_recipe', $post_type ) ){
+            		$post_type[] = 'rpr_recipe';
+            	} else {
+            		$post_type = array( 'post', 'rpr_recipe' );
+            	}
+            	$query->set( 'post_type', $post_type );
             }
 
             // No idea why we neeed(ed) this. However it is interfering in the search preventing other cpt to be found!
