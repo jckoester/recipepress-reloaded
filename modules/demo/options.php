35853df5369155605a71395092a1a43b53acb033
Jan Köster <dasmaeh@cbjck.de>
2017-08-27 23:40:57 +0200

    Improved options of module demo

--- modules/demo/options.php
+++ modules/demo/options.php
@@ -42,56 +42,60 @@ class RPR_Options_Page_Metadata_Demo {
      */
     public function __construct( $oFactory ) {
     
-        // Section
+        // Add an option section to an existing tab
         $oFactory->addSettingSections(    
             $this->sPageSlug, // the target page slug   
             array(
-                'section_id'    => $this->sSectionID,
-                'tab_slug'      => $this->sTabSlug,
+                'section_id'    => 'metadata_demo',
+                'tab_slug'      => 'metadata',
                 'title'         => '<i class="fa fa-tags"></i> ' . __( 'Demo Settings' , 'recipepress-reloaded' )
                 )
             );
       
         /**
-        * Add settings fields for nutritional metadata
+        * Add option fields to the new secion
         */
         $oFactory->addSettingFields(
             $this->sSectionID, 
             array(
-                'field_id'          => 'use_nutritional_data',
+                'field_id'          => 'demo_use_nutritional_data',
                 'type'              => 'checkbox',
-                'title'             => __( 'Use nutritional meta data', 'recipepress-reloaded' ),
-                'tip'               => __( 'Check this to enable the use of nutritional information like calorific value, protein, fat and carbohydrates.', 'recipepress-reloaded'),
-                'default'			=> false,
+                'title'             => __( 'Demo module checkbox option in foreign tab.', 'recipepress-reloaded' ),
+                //'tip'               => __( 'Check this to enable the use of nutritional information like calorific value, protein, fat and carbohydrates.', 'recipepress-reloaded'),
+                'default'           => false,
             ),
-			array(
-				'field_id'			=> 'structured_data_format',
-				'type'				=> 'select',
-				'title'				=> __( 'Structured data format', 'recipepress-reloaded' ),
-				'description'				=> sprintf( __( 'Structured data help search engines understand your content. Find more information on structured data formats at <a href="%1s" target="_blank">schema.org</a>', 'recipepress-reloaded' ), 'http://1.schemaorgae.appspot.com/Recipe' ),
-				'tip'				=> __( 'Structured data help search engines understand your content. Find more information on structured data formats at schema.org.', 'recipepress-reloaded' ),
-				'label'				=> array(
-					'microdata'	=> __( 'Microdata', 'recipepress-reloaded' ),
-					'rdfa'		=> __( 'RDFa', 'reciperess-reloaded' ),
-					'json-ld'	=> __( 'JSON-LD', 'reciperess-reloaded' )
-				),
-				'default'			=> 'microdata'
-			)
+            array(
+		'field_id'			=> 'demo_structured_data_format',
+		'type'				=> 'select',
+		'title'				=> __( 'Demo module select option in foreign tab', 'recipepress-reloaded' ),
+		//'description'			=> sprintf( __( 'Structured data help search engines understand your content. Find more information on structured data formats at <a href="%1s" target="_blank">schema.org</a>', 'recipepress-reloaded' ), 'http://1.schemaorgae.appspot.com/Recipe' ),
+		//'tip'				=> __( 'Structured data help search engines understand your content. Find more information on structured data formats at schema.org.', 'recipepress-reloaded' ),
+		'label'				=> array(
+                    'option1'	=> __( 'Option 1', 'recipepress-reloaded' ),
+                    'option2'	=> __( 'Option 2', 'reciperess-reloaded' ),
+                    'option3'	=> __( 'Option 3', 'reciperess-reloaded' )
+		),
+		'default'			=> 'option2'
+            )
         );
         
-        //Works!
-        // @todo: how to set the place, where in the section the option appears?
-        // Add settings to other section:
+
+        // Add settings to existing section:
         $oFactory->addSettingFields(
             'general', 
             array(
                 'field_id'          => 'test_demo_option',
                 'type'              => 'checkbox',
                 'title'             => __( 'This is a demo option', 'recipepress-reloaded' ),
+                'description'       => __( 'This is an option from the demo module. If this option appears, dynamic loading of module option does work!', 'recipepress-reloaded'),
                 'tip'               => __( 'If this works, dynamic loading of module option does work!', 'recipepress-reloaded'),
-                'default'			=> false,
+                'default'           => false,
             )
         );
+        
+        // it should also be possible to create a new options tab:
+        // You will need to create a new class for this
+        // Anyhow it is not recommended to create new tabs for modules as this crowds the options page
     }
 
 }
